ad863d7aff62ece7d408277d927ba1e0b6556031
Steven Ott <stevenkott@icloud.com>
2018-11-03 22:33:24 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,6 +5,7 @@ def fizzbuzz(int)
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-    
-end# Don't forget! This file needs to be 'required' in its spec file
+  end  
+end
+# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
